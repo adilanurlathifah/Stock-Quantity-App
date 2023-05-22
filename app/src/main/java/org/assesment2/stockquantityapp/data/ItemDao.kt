f45bb1364484c0503ec075c4efce5f9ffa7bed0d
Adila Nur Lathifah <strawberrytata3031@gmail.com>
2023-05-22 15:45:15 +0700

    membuat database dengan Room

---
+++ app/src/main/java/org/assesment2/stockquantityapp/data/ItemDao.kt
@@ -0,0 +1,23 @@
+package org.assesment2.stockquantityapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ItemDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addItem(item: Item)
+
+    @Update
+    suspend fun updateItem(item: Item)
+
+    @Delete
+    suspend fun deleteItem(item: Item)
+
+    @Query("DELETE FROM item_table")
+    fun deleteAllItems()
+
+    @Query("SELECT * FROM item_table ORDER BY id ASC")
+    fun readAllData():LiveData<List<Item>>
+}
