3f4933e3a50bf0b68206f8e6ab2946c04a19ceda
Adila Nur Lathifah <strawberrytata3031@gmail.com>
2023-05-22 17:07:55 +0700

    Menambah class repository serta membuat view model untuk stock item

---
+++ app/src/main/java/org/assesment2/stockquantityapp/viewmodel/ItemViewModel.kt
@@ -0,0 +1,49 @@
+package org.assesment2.stockquantityapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.assesment2.stockquantityapp.data.ItemDatabase
+import org.assesment2.stockquantityapp.model.Item
+import org.assesment2.stockquantityapp.repository.ItemRepository
+
+class ItemViewModel(application: Application) : AndroidViewModel(application) {
+
+    val readAllData : LiveData<List<Item>>
+    private val repository : ItemRepository
+
+    init{
+        val itemDao = ItemDatabase.getDatabase(application).itemDao()
+        repository = ItemRepository(itemDao)
+        readAllData = repository.readAllData
+    }
+
+    fun addItem(item: Item){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addItem(item)
+        }
+    }
+
+    fun updateItem(item: Item)
+    {
+        viewModelScope.launch(Dispatchers.IO){
+            repository.updateItem(item)
+        }
+    }
+
+    fun deleteItem(item: Item){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.deleteItem(item)
+        }
+    }
+
+
+    fun deleteAllItem(){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.deleteAllItem()
+        }
+    }
+}
